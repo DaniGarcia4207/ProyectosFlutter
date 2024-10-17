0349b8c260376bd855b4c02327b7e6b588538446
DaniGarcia4207 <dglr628@gmail.com>
2024-10-17 10:39:16 -0500

    yes_no_app

--- calculadora/lib/main.dart
+++ calculadora/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:calculadora/calculator.dart';
-
+//import 'package:calculadora/calculator.dart';
+import 'package:calculadora/presentacion/pantallas/chat/chat_pantalla.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,14 +12,23 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    // return MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   theme: ThemeData(
+    //     useMaterial3: true,
+    //     colorSchemeSeed: Colors.blue
+    //   ),
+    //    home: const Calculator(),
+    // );     
 
+    return MaterialApp(
+      title: 'Yes No App',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
         useMaterial3: true,
         colorSchemeSeed: Colors.blue
       ),
-       home: const Calculator(),
+       home: const ChatPantalla(),
     );     
  }
 }
